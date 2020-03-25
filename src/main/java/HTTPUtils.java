3751b5f118717928d83bfb0a43a0bfa62c4d7efa
ColeNewkerk <crnewkerk@gmail.com>
2020-03-25 12:54:47 -0400

    commit

--- src/main/java/HTTPUtils.java
+++ src/main/java/HTTPUtils.java
@@ -21,9 +21,14 @@ public class HTTPUtils {
      * @throws IOException
      */
     public String getTodoItemJsonString(int id) throws IOException {
+
         // TODO
-        return "";
+        HttpRequest getRequest = requestFactory.buildGetRequest(
+                new GenericUrl("https://todoserver222.herokuapp.com/todos/1"));
+        String rawResponse = getRequest.execute().parseAsString();
+        return rawResponse;
     }
+
 
     /**
      * @param note  whatever should be in the todoItem
@@ -33,7 +38,17 @@ public class HTTPUtils {
      */
     public int addTodoItem(String note, String owner) throws IOException {
         // TODO
-        return -1;
+        Map<String, Object> data = new LinkedHashMap<>();
+        data.put("title", "HelloPostRequest");
+        data.put("owner", "hergin");
+        HttpContent content = new UrlEncodedContent(data);
+        HttpRequest postRequest = requestFactory.buildPostRequest(
+                new GenericUrl("https://todoserver222.herokuapp.com/todos"),content);
+        String rawResponse = postRequest.execute().parseAsString();
+        int id = rawResponse.getInt("id");
+        return id;
+
+
     }
 
     /**
@@ -43,7 +58,16 @@ public class HTTPUtils {
      */
     public boolean deleteTodoItem(int id) throws IOException {
         // TODO
-        return false;
+        HttpRequest deleteRequest = requestFactory.buildDeleteRequest(
+                new GenericUrl("https://todoserver222.herokuapp.com/todos/3"));
+        String rawResponse = deleteRequest.execute().parseAsString();
+       if(rawResponse == ""){
+           return true;
+       }
+       else{
+           return false;
+       }
+
     }
 
 }

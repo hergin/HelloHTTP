efc1d26f475a5666395b632d39de015d8b78d262
Huseyin Ergin <hergin@bsu.edu>
2020-03-25 11:30:45 -0400

    Initial files

---
+++ src/main/java/HTTPUtils.java
@@ -0,0 +1,49 @@
+import com.google.api.client.http.*;
+import com.google.api.client.http.javanet.NetHttpTransport;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class HTTPUtils {
+
+    HttpRequestFactory requestFactory;
+    String baseURL = "https://todoserver222.herokuapp.com/";
+    String todosURL = baseURL + "todos/";
+
+    public HTTPUtils() {
+        requestFactory = new NetHttpTransport().createRequestFactory();
+    }
+
+    /**
+     * @param id
+     * @return JSON string of the todoItem with id
+     * @throws IOException
+     */
+    public String getTodoItemJsonString(int id) throws IOException {
+        // TODO
+        return "";
+    }
+
+    /**
+     * @param note  whatever should be in the todoItem
+     * @param owner whoever is the owner of the todoItem
+     * @return the ID of the recently added todoItem
+     * @throws IOException
+     */
+    public int addTodoItem(String note, String owner) throws IOException {
+        // TODO
+        return -1;
+    }
+
+    /**
+     * @param id of the todoItem to delete
+     * @return true if succesfully deleted. Otherwise false.
+     * @throws IOException
+     */
+    public boolean deleteTodoItem(int id) throws IOException {
+        // TODO
+        return false;
+    }
+
+}
---
+++ src/test/java/HTTPUtilsTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HTTPUtilsTest {
+
+    HTTPUtils httpUtils;
+
+    @BeforeEach
+    void setup() {
+        httpUtils = new HTTPUtils();
+    }
+
+    @Test
+    void getTodoItemJsonString() throws IOException {
+        var result = httpUtils.getTodoItemJsonString(1);
+        assertEquals("{\n" +
+                "  \"id\": 1,\n" +
+                "  \"title\": \"Explain the project\",\n" +
+                "  \"owner\": \"hergin\"\n" +
+                "}", result);
+    }
+
+    @Test
+    void addTodoItem() throws IOException {
+        var resultingID = httpUtils.addTodoItem("Hello", "hergin");
+        var expected = "{\n" +
+                "  \"title\": \"Hello\",\n" +
+                "  \"owner\": \"hergin\",\n" +
+                "  \"id\": " + resultingID + "\n" +
+                "}";
+        var actual = httpUtils.getTodoItemJsonString(resultingID);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void deleteExistingTodoItem() throws IOException {
+        var resultingID = httpUtils.addTodoItem("Hello", "hergin");
+        var deleteResult = httpUtils.deleteTodoItem(resultingID);
+        assertTrue(deleteResult);
+    }
+
+    @Test
+    void deleteNotExistingTodoItem() throws IOException {
+        var nonExistingIDdeleteResult = httpUtils.deleteTodoItem(152434354);
+        assertFalse(nonExistingIDdeleteResult);
+    }
+
+}
